2d59daf1751287cf6edf27a953df487f9ed21482
callanos <callxip@gmail.com>
2022-02-11 14:03:26 -0500

    Construcción de estructura front

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import {BrowserRouter as Router, Routes, Route, Redirect} from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/register" component={ Register }/>
+        <Route path="/login" component={ Login }/>
+        <Route path="/logout" component={ Logout }/>
+      </Routes>
+    </Router>
   );
 }
 
