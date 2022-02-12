982e1c37eb0f5432d030f80bdf1ae1e7ff05afdf
callanos <callxip@gmail.com>
2022-02-12 10:01:22 -0500

    adición formulario register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,8 @@ import NavigationBar from '../components/NavigationBar'
 import { useAuth } from  '../contexts/AuthContext'
 
 async function Login() {
-  //const { login } = useAuth();
-  //console.log(await login('user4@email.com', 'abc1234!'))
+  const { login } = useAuth();
+  console.log(await login('user4@email.com', 'abc1234!'))
 
   return (
     <>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,14 +1,42 @@
 import NavigationBar from '../components/NavigationBar'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
 import { useAuth } from  '../contexts/AuthContext'
 
 function Register() {
-  const { register } = useAuth();
-  console.log(register('user4@email.com', 'abc1234!'))
+  //const { register } = useAuth();
+  //console.log(register('user4@email.com', 'abc1234!'))
 
   return (
     <>
       <NavigationBar />
-      <h1>REGISTER</h1>
+      <Container className="pt-5 d-flex align-items-center justify-content-center">
+      <Row className="pt-5">
+        <Col className="pt-5">
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>    
     </>
   )
 }
