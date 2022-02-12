e12421193d043bb3c1f767b3e7cb422d4973251d
callanos <callxip@gmail.com>
2022-02-12 10:02:31 -0500

    adición check is a doctor

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -30,6 +30,9 @@ function Register() {
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
               </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Is a Doctor" />
+              </Form.Group>
               <Button variant="primary" type="submit">
                 Submit
               </Button>
